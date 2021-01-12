598de9cbc091a8ada03e06056b4da3ca52ff01c4
Артем Лазарчик <lazarchik_2000@mail.ru>
2021-01-12 12:34:16 +0200

    Добавьте файлы проекта.

---
+++ Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите часы: ");
+            int hour = int.Parse(Console.ReadLine());
+            Console.Write("Введите минуты: ");
+            int minute = int.Parse(Console.ReadLine());
+            Console.Write("Введите секунды: ");
+            int seconds = int.Parse(Console.ReadLine());
+            Time time = new Time();
+            time.Add(hour,minute,seconds);
+            time.Print();
+            time.Change();
+            time.Print();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Classes/Time.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Time
+    { 
+        public int hours;
+        public int minutes;
+        public int seconds;
+        public void Add(int hour, int minute, int seconds)
+        {
+            this.hours = hour;
+            this.minutes = minute;
+            this.seconds = seconds;
+        }
+        public void Change()
+        {
+            this.hours += 1;
+            this.minutes += 40;
+            if (this.minutes >= 60)
+            {
+                this.hours += this.minutes / 60;
+                this.minutes -= (this.minutes / 60 * 60);
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Время: {hours}.{minutes}.{seconds} До полуночи (24:00:00) - {24 * 60 - (hours * 60 + minutes)}.{seconds} минут");
+        }
+    }
+}
